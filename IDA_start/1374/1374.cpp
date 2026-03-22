fc7c6306c99ae3554acd4f29a578357158796eb3
jimlin2004 <jim.lin2004@gmail.com>
2026-03-22 19:05:06 +0800

    2026 03 22

---
+++ IDA_start/1374/1374.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    題意是指要算出x^n，可以用最少多少次乘法與除法算出來?
+    所以每次乘的時候使用算過的，除的時候也是
+
+    解法:
+    這題沒有很好的辦法greedy或dp算出最優次數
+    所以改用dfs的方式暴力搜尋
+    但dfs無法保證最優解，也需要剪枝加速
+    所以正確做法是使用IDA*
+    每一次增加一點深度直到搜到第一個解，那個解一定是最少步數的(跟bfs同理)
+
+    詳細見程式
+*/
+
+int maxd; // 目前搜尋的最深深度限制
+int n; // 題目的x^n
+
+// dfs路徑上每個node算到多少x^p
+// 如pathNodes[0] -> 深度為0的那個路徑node算到多少x^p，pathNodes[1] -> 深度為1的那個路徑node算到多少x^p
+int pathNodes[30]; 
+
+bool dfs(int depth)
+{
+    // 如果到了深度限制，檢查是不是算出x^n了
+    if (depth == maxd)
+    {
+        return pathNodes[depth] == n;
+    }
+
+    // 剪枝技巧:
+    // 在嘗試dfs搜尋的時候，如果目前這個點答案(x^p)用最快增加冪次的方式(不斷平方操作)
+    // 仍然 x^p < x^n，那這個點就永遠不可能算出答案
+    if ((pathNodes[depth] << (maxd - depth)) < n)
+    {
+        return false;
+    }
+
+    // 往回看之前dfs路徑上曾經算過哪些冪次
+    for (int i = depth; i >= 0; --i)
+    {
+        // 下一層試試看用 x^(pathNodes[depth] + pathNodes[i])的
+        pathNodes[depth + 1] = pathNodes[depth] + pathNodes[i];
+        // 限制在1000是題目資料範圍，也算是剪枝
+        if (pathNodes[depth + 1] <= 1000 && dfs(depth + 1))
+            return true;
+        
+        // 下一層試試看用 x^(pathNodes[depth] + pathNodes[i])的
+        pathNodes[depth + 1] = pathNodes[depth] - pathNodes[i];
+        // 題目說過程中不能有負的冪次，也算是剪枝
+        if (pathNodes[depth + 1] >= 0 && dfs(depth + 1))
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    while (scanf("%d", &n) && n)
+    {
+        if (n == 1)
+        {
+            printf("0\n");
+            continue;
+        }
+
+        // 第一層一定是x^1的
+        pathNodes[0] = 1;
+        maxd = 1;
+
+        // 開始IDA*暴力搜尋
+        while (true)
+        {
+            if (dfs(0))
+            {
+                printf("%d\n", maxd);
+                break;
+            }
+
+            ++maxd;
+        }
+    }
+    return 0;
+}
+
+/*
+*/
---
+++ Simulate/10366/10366.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/*
+    很麻煩的模擬題
+    1. 觀察到水量等於需要的時間
+    2. 找左邊最高高度LH，右邊最高高度RH
+    3. 以LH跟RH的高度比較確認最終是左邊溢出還是右邊溢出或是兩邊溢出(分情況討論)
+    4. 分情況討論時，分成左邊水池vL、中間水池vCenter、右邊水池vR去計算答案
+    細節看下方程式
+*/
+
+int leftx, rightx;
+int Lheights[1005];
+int Rheights[1005];
+int numL, numR;
+int LH, RH; // 左邊最高以及右邊最高的高度
+int reqL[1005]; // 左邊要溢出時需要的水量
+int reqR[1005]; // 右邊要溢出時需要的水量
+
+
+int main()
+{
+    while (scanf("%d %d", &leftx, &rightx))
+    {
+        if (leftx == 0 && rightx == 0)
+            break;
+
+        numL = (-1 - leftx) / 2 + 1;
+        for (int i = 0; i < numL; ++i)
+            scanf("%d", &Lheights[numL - i - 1]); // Lheights要倒過來存比較方便
+
+        numR = (rightx - 1) / 2 + 1;
+        for (int i = 0; i < numR; ++i)
+            scanf("%d", &Rheights[i]);
+
+        // 找到從0到左最高的高度
+        LH = *max_element(Lheights, Lheights + numL);
+        // 找到從0到右最高的高度
+        RH = *max_element(Rheights, Rheights + numR);
+
+        // 計算出左邊要溢出時，每一個區間會有的水量
+        // 要往左邊溢出，該區間的水位需要高於左邊所有檔板的最高高度
+        reqL[numL - 1] = Lheights[numL - 1];
+        for (int i = numL - 2; i >= 0; --i)
+            reqL[i] = max(Lheights[i], reqL[i + 1]);
+        
+        // 計算出右邊要溢出時，每一個區間會有的水量
+        // 要往右邊溢出，該區間的水位需要高於右邊所有檔板的最高高度
+        reqR[numR - 1] = Rheights[numR - 1];
+        for (int i = numR - 2; i >= 0; --i)
+            reqR[i] = max(Rheights[i], reqR[i + 1]);
+        
+        // 找出兩邊各自第一個能擋住高度為H的檔板，就是中間水池的邊界
+        int H = min(LH, RH);
+        int LCenterBound = 0;
+        while (LCenterBound < numL && Lheights[LCenterBound] < H)
+            ++LCenterBound;
+        int RCenterBound = 0;
+        while (RCenterBound < numR && Rheights[RCenterBound] < H)
+            ++RCenterBound;
+        
+        // 中間水池的水量
+        int vCenter = H * (LCenterBound + RCenterBound + 1) * 2;
+
+        int totalTime = 0;
+
+        if (LH > RH) // 最後是右邊溢出
+        {
+            int vR = 0;
+            for (int i = RCenterBound + 1; i < numR; ++i)
+                vR += reqR[i] * 2;
+
+            int LStop = LCenterBound;
+            // 算算看左邊最多會溢出多少，也就是撞到 > H的檔板為止
+            while (LStop < numL && Lheights[LStop] <= H)
+                ++LStop;
+            int vL = (LStop - LCenterBound) * H * 2;
+
+            // 右邊不一定流滿，所以用min(vL, vR)
+            totalTime = vR + vCenter + min(vL, vR);
+        }
+        else if (LH < RH) // 最後是左邊溢出
+        {
+            int vL = 0;
+            for (int i = LCenterBound + 1; i < numL; ++i)
+                vL += reqL[i] * 2;
+
+            int RStop = RCenterBound;
+            // 算算看右邊最多會溢出多少，也就是撞到 > H的檔板為止
+            while (RStop < numR && Rheights[RStop] <= H)
+                ++RStop;
+            int vR = (RStop - RCenterBound) * H * 2;
+
+            // 左邊不一定流滿，所以用min(vL, vR)
+            totalTime = vL + vCenter + min(vL, vR);
+        }
+        else // 兩邊一樣高
+        {
+            // 左邊額外的水量
+            int vL = 0;
+            for (int i = LCenterBound + 1; i < numL; ++i)
+                vL += reqL[i] * 2;
+
+            // 右邊額外的水量
+            int vR = 0;
+            for (int i = RCenterBound + 1; i < numR; ++i)
+                vR += reqR[i] * 2;
+            
+            // min是因為選擇先溢出的水量
+            totalTime = vCenter + 2 * min(vL, vR);
+        }
+
+        printf("%d\n", totalTime);
+    }
+
+    return 0;
+}
